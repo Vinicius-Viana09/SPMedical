f2f38127d9ffdd79615145f7ac20889377475c9d
ViniciusVianazin <viniciusniviana@gmail.com>
2021-09-26 17:43:35 -0300

    começo

---
+++ API/SpMedical_WebApi/Domains/Consultum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SpMedical_WebApi.Domains
+{
+    public partial class Consultum
+    {
+        public byte IdConsulta { get; set; }
+        public byte? IdMedico { get; set; }
+        public byte? IdPaciente { get; set; }
+        public string DataConsulta { get; set; }
+        public string Situacao { get; set; }
+
+        public virtual Medico IdMedicoNavigation { get; set; }
+        public virtual Paciente IdPacienteNavigation { get; set; }
+    }
+}
